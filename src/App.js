d730a2ed207098a26712ee03800fc93f6290fc45
Emmanuel Mendieta <emmanuel_mendieta@icloud.com>
2023-02-24 20:27:38 -0600

    Home/Login/Create user /Dashboard pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Account from './pages/account.js';
+import Calculator from './pages/calculator.js';
+import Create from './pages/create.js';
+import Dashboard from './pages/dashboard.js';
+import History from './pages/history.js';
+import Home from './pages/home.js';
+import Login from './pages/login.js';
+
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Home/>} />
+        <Route path="/account" element = {<Account/>} />
+        <Route path="/calculator" element = {<Calculator/>} />
+        <Route path="/create" element = {<Create/>} />
+        <Route path="/dashboard" element = {<Dashboard/>} />
+        <Route path="/history" element = {<History/>} />
+        <Route path="/login" element = {<Login/>} />
+
+
+      </Routes>
+    </Router>
   );
 }
 
